63f637c4fcadf13c1d0c495a5afb5632d5b96319
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-15 17:59:06 +0200

    create get and post methods

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,7 +1,37 @@
 import { db } from '@/server/db';
+import { v4 as uuid } from 'uuid';
+import { sendError } from 'h3';
 
-export default defineEventHandler((e) => {
-  const methods = e.node.req.method;
-  
-  return db.todos;
+export default defineEventHandler(async (e) => {
+  const method = e.node.req.method;
+
+  if (method === 'GET') {
+    console.log(db.todos);
+    return db.todos;
+  }
+
+  if (method === 'POST') {
+    const body = await readBody(e);
+    console.log(db.todos);
+
+    if (!body.item) {
+      const TodoNotFoundError = createError({
+        statusCode: 400,
+        statusMessage: 'No item provided',
+        data: {},
+      });
+
+      sendError(e, TodoNotFoundError);
+    }
+
+    const newTodo = {
+      id: uuid(),
+      item: body.item,
+      completed: false,
+    };
+
+    db.todos.push(newTodo);
+
+    return newTodo;
+  }
 });
